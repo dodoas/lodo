6ffa61bcdb55eb5eb5a514e34af94d9b68532d84
Paal de Vibe <paalvibe@frisurf.no>
2010-04-06 12:40:55 -0430

    removed duration field as it hasnt been added to db table logaccessdenied yet

--- code/lib/log/log.class.php
+++ code/lib/log/log.class.php
@@ -99,6 +99,8 @@ class logg {
     $fields['logaccessdenied_Template']   = $_lib['sess']->get_session('Template');
     $fields['logaccessdenied_Module']     = $_lib['sess']->get_session('Module');
     $fields['logaccessdenied_Interface']  = $_lib['sess']->get_session('Interface');
+    # comment in duration when it has been added as a valid field
+    # $fields['logaccessdenied_Duration']   = $args['duration'];
     $fields['logaccessdenied_Duration']   = $args['duration'];
     $fields['logaccessdenied_SessionID']  = $_lib['sess']->get_session('SID');
     $fields['logaccessdenied_PersonID']   = $_lib['sess']->get_person('PersonID');
