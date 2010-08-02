5d0f578109d4073b0255136aeab92e0fb0b94a61
Paal de Vibe <paalvibe@frisurf.no>
2010-08-02 15:55:19 -0430

    fixed typo in script name

--- db/changes/025_expand_fakturabank_tables.sql
+++ db/changes/025_expand_fakturabank_tables.sql
@@ -1,6 +1,6 @@
 -- Add new fakturabank fields to lodo's fakturabank data holding tables
 
-ALTER TABLE `fakturabanktransaction` ADD COLUMN `Invoiceno` varchar(255) DEFAULT NULL AFTER `KID`;
+ALTER TABLE `fakturabanktransaction` ADD COLUMN `InvoiceNumber` varchar(255) DEFAULT NULL AFTER `KID`;
 ALTER TABLE `fakturabanktransaction` ADD COLUMN `IsSplit` tinyint(1) DEFAULT NULL AFTER `InvoiceNumber`;
 ALTER TABLE `fakturabanktransaction` ADD COLUMN `ParentID` bigint(20) DEFAULT NULL AFTER `IsSplit`;
 ALTER TABLE `fakturabanktransaction` ADD COLUMN `TransactionType` varchar(2) DEFAULT NULL AFTER `ParentID`;
