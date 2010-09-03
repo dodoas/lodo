14b7faff494094f482f7d045aa930879c8027508
martin <martin@blixsolutions.no>
2010-09-03 16:08:58 +0200

    Resize field

--- db/changes/026_add_salaryinfo.sql
+++ db/changes/026_add_salaryinfo.sql
@@ -3,6 +3,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `salaryinfo` (
   `SalaryInfoID` int(11) NOT NULL auto_increment,
   `SalaryConfID` int(11) NOT NULL,
-  `amount` char(10) NOT NULL,
+  `amount` char(128) NOT NULL,
   PRIMARY KEY  (`SalaryInfoID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
