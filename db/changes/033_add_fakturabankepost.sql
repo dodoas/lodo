6c46f8438042748d6b453239a505ff585653b137
martin <martin@blixsolutions.no>
2010-11-05 16:18:10 +0100

    Added db-scripts

---
+++ db/changes/033_add_fakturabankepost.sql
@@ -0,0 +1,5 @@
+CREATE TABLE  `BLIX_SOLUTIONS_AS`.`fakturabankemail` (
+`AccountPlanID` INT( 11 ) NOT NULL ,
+`Email` CHAR( 255 ) NOT NULL ,
+PRIMARY KEY (  `AccountPlanID` )
+) ENGINE = MYISAM
---
+++ db/changes/035_modify_add_field_salary_comment.sql
@@ -0,0 +1,1 @@
+ALTER TABLE  `salary` ADD  `Comment` TEXT NOT NULL ;
