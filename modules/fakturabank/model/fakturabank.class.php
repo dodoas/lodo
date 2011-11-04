e892bc6669ce7b56ae095ee6b1550bd51ac1c768
Paal de Vibe <paalvibe@frisurf.no>
2011-11-04 14:22:16 -0430

    reduced max rows

--- modules/fakturabank/model/fakturabank.class.php
+++ modules/fakturabank/model/fakturabank.class.php
@@ -73,7 +73,7 @@ class lodo_fakturabank_fakturabank {
 
         $page       = "invoices/outgoing.xml";
 
-        $params     = "?rows=1000&orgnr=$this->OrgNumber"; // add top limit rows=1000, otherwise we only get one record
+        $params     = "?rows=200&orgnr=$this->OrgNumber"; // add top limit rows=1000, otherwise we only get one record
         $params     .= "&supplier_status=created"; #Only retrieve with status 'created'
         $params     .= "&order=invoiceno";
         
@@ -93,7 +93,7 @@ class lodo_fakturabank_fakturabank {
         #https://fakturabank.no/invoices?orgnr=981951271
 
         $page       = "invoices";
-        $params     = "?rows=1000&orgnr=" . $this->OrgNumber . '&order=issue_date'; // add top limit rows=1000, otherwise we only get one record
+        $params     = "?rows=200&orgnr=" . $this->OrgNumber . '&order=issue_date'; // add top limit rows=1000, otherwise we only get one record
         if($this->retrievestatus) $params .= '&customer_status=' . $this->retrievestatus;
         $url    = "$this->protocol://$this->host/$page$params";
         $_lib['message']->add($url);
