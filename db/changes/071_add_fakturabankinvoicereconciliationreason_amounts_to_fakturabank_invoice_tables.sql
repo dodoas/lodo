7fcebc6ead3e6e73aa7ed7101af51d8e27694d2a
Paal de Vibe <paalvibe@frisurf.no>
2012-06-20 09:28:25 -0430

    add reason amount to fakturabank invoice tables

---
+++ db/changes/071_add_fakturabankinvoicereconciliationreason_amounts_to_fakturabank_invoice_tables.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `fakturabankinvoicein` ADD `FakturabankCustomerReconciliationReasonAmount` FLOAT;
+ALTER TABLE `fakturabankinvoicein` ADD `FakturabankSupplierReconciliationReasonAmount` FLOAT;
+ALTER TABLE `fakturabankinvoiceout` ADD `FakturabankCustomerReconciliationReasonAmount` FLOAT;
+ALTER TABLE `fakturabankinvoiceout` ADD `FakturabankSupplierReconciliationReasonAmount` FLOAT;
