da33eb5b13d0446db4dd15675524b1ba9e9c11b2
martin <martin@blixsolutions.no>
2012-06-29 10:57:49 +0000

    Changes how closing vouchers work. Adds MatchID/MatchNumber for manual overriding

---
+++ db/changes/072_add_matchid.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `vouchermatch` (
+`VoucherMatchID` INT( 11 ) NOT NULL AUTO_INCREMENT ,
+`VoucherID` INT( 11 ) NOT NULL ,
+`MatchNumber` INT( 11 ) NOT NULL ,
+PRIMARY KEY (  `VoucherMatchID` )
+) ENGINE = MYISAM ;
