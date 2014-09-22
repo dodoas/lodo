a3d989cfd2518e129ee2fbdb35e69937c0de4baa
Ivan <peacemaker.994@gmail.com>
2014-09-22 08:27:49 +0200

    go back to the old mail function

--- modules/invoice/view/sendmail.php
+++ modules/invoice/view/sendmail.php
@@ -1,17 +1,15 @@
 <?php
 
-require('mailsend.php');
 //$_RESQUEST['InvoiceID']=11245;
 //error_reporting(E_ALL);
 
-function send_invoice($to, $from, $invoiceno, $html, $attachment, $subject) {
+function send_invoice($to, $from, $invoiceno, $html, $attachment) {
     global $_lib;
     $attachment = str_replace("\r", "", chunk_split(base64_encode($attachment)));
     $html = str_replace(array("=", "\r"), array("=3D", ""), $html);
     $hash = md5(time());
 
     $boundary = $hash;
-    $firstb = $boundary;
 
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: multipart/mixed; boundary='.$boundary. "\r\n";
@@ -34,15 +32,26 @@ function send_invoice($to, $from, $invoiceno, $html, $attachment, $subject) {
                        $invoiceno, $invoiceno,
                        $attachment);
 
-    $mailer = new Mailer();
-    $mailer->send("admin@lodo.no", $to, $subject, $message, $firstb);
+    mail(
+          $to
+        , $_lib['sess']->company->CompanyName . " - invoice " . $invoiceno
+        , $message
+        , $headers
+    );
 }
 
+ob_start();
+include('print.php');
+$data_html = ob_get_contents();
+ob_end_clean();
 ob_start();
 include('print2.php');
 header('Content-type: text/html;');
 $data_pdf = ob_get_contents();
 ob_end_clean();
+
+$data_html = "<html><body>" . strstr($data_html, "<h2>");
+$data_html = strip_tags($data_html, "<html><body><table><h2><tr><td><thead><tbody><tfoot><label><colgroup><br><th>");
 
 $recipient = $_REQUEST['email_recipient'];
 
@@ -51,17 +60,8 @@ if(isset($_REQUEST['send_mail_copy']) && $_REQUEST['send_mail_copy'])
   $recipient .= ', ' . $_REQUEST['send_mail_copy_mail'];
 }
 
-$InvoiceID = $_REQUEST['InvoiceID'];
-$db_table = "invoiceout";
-$get_invoice            = "select I.*, A.InvoiceCommentCustomerPosition from $db_table as I, accountplan as A where InvoiceID='$InvoiceID' and A.AccountPlanID=I.CustomerAccountPlanID";
-$row                    = $_lib['storage']->get_row(array('query' => $get_invoice));
 
-$data_html = "Her er faktura fra " . $row->SName . " til " . $row->IName . ".<br>";
-$data_html .= "Faktura med fakturanummer: " . $InvoiceID . "<br>";
-$data_html .= "Se vedlegg for " . iconv("UTF-8", "ISO-8859-1", 'å') . " se hele fakturaen";
-
-send_invoice($recipient, $row_from->Email, $_REQUEST['InvoiceID'], $data_html, $data_pdf, $row->SName . " - faktura " . $InvoiceID);
-
+send_invoice($recipient, $row_from->Email, $_REQUEST['InvoiceID'], $data_html, $data_pdf);
 echo $recipient;
 echo '<h2>Email sent</h2>';
 echo '<META HTTP-EQUIV="Refresh" CONTENT="1; URL='. $_lib['sess']->dispatch . 't=invoice.edit&InvoiceID=' . $InvoiceID . '">';
