41b17bcb12667137594aee6d5fd504253b5509da
Mladen Drobnjakovic <mladjo2505@gmail.com>
2015-03-23 13:01:49 +0100

    Yearly salary report, small fix.

--- modules/salary/model/salaryreport.class.php
+++ modules/salary/model/salaryreport.class.php
@@ -76,15 +76,19 @@ class salaryreport
         
         $this->_reportHash['head']['000']['sumLineCode'] = $result["SkyldigFeriepengeGrunnlag"];
         
-        $this->_query = "select sum(sl.AmountThisPeriod) as sumLineCode, sl.SalaryCode, sl.SalaryText, sum(sl.NumberInPeriod) as NumberInPeriod, sl.LineNumber from salary as s, salaryline as sl where s.AccountPlanID='$this->_personID' and s.SalaryID=sl.SalaryID and substring(s.Period, 1, 4)='$this->_year' and sl.SalaryCode is not null and sl.SalaryCode != '' group by sl.SalaryText, sl.LineNumber";
-        $this->_salaryLineHash = $_lib['storage']->get_hashhash(array('query'=>$this->_query, 'key'=>'SalaryText'));
-        foreach($this->_salaryLineHash as $SalaryText => $lineHash)
+        $this->_query = "select sl.SalaryLineID, sum(sl.AmountThisPeriod) as sumLineCode, sl.SalaryCode, sl.SalaryText, sum(sl.NumberInPeriod) as NumberInPeriod, sl.LineNumber from salary as s, salaryline as sl where s.AccountPlanID='$this->_personID' and s.SalaryID=sl.SalaryID and substring(s.Period, 1, 4)='$this->_year' and sl.SalaryCode is not null and sl.SalaryCode != '' group by sl.SalaryText, sl.LineNumber";
+        $this->_salaryLineHash = $_lib['storage']->get_hashhash(array('query'=>$this->_query, 'key'=>'SalaryLineID'));
+        foreach($this->_salaryLineHash as $SalaryLineID => $lineHash)
         {
         	$salaryCode = $lineHash["SalaryCode"];
             if($this->_salaryTopLineHash[$salaryCode] != 1)
             {
                 //$this->_salaryBottomLineHash[$salaryCode] = $lineHash;
-                $this->_reportHash['body'][$lineHash['LineNumber']] = $lineHash;
+                $this->_reportHash['body'][$lineHash['LineNumber']]['sumLineCode'] = $lineHash['sumLineCode'];
+                $this->_reportHash['body'][$lineHash['LineNumber']]['SalaryCode'] = $lineHash['SalaryCode'];
+                $this->_reportHash['body'][$lineHash['LineNumber']]['SalaryText'] = $lineHash['SalaryText'];
+                $this->_reportHash['body'][$lineHash['LineNumber']]['NumberInPeriod'] = $lineHash['NumberInPeriod'];
+                $this->_reportHash['body'][$lineHash['LineNumber']]['LineNumber'] = $lineHash['LineNumber'];
                 if (!isset($this->_reportHash['body'][$lineHash['LineNumber']]['MultipleLines'])) {
                   $this->_reportHash['body'][$lineHash['LineNumber']]['MultipleLines'] = false;
                   $this->_reportHash['body'][$lineHash['LineNumber']]['MultipleLinesNumber'] = 0;
