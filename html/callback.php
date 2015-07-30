294d8cbd95c905d66d53be66dfb1a4ffd171e8b3
Mladen Drobnjakovic <mladjo2505@gmail.com>
2015-07-30 16:01:18 +0200

    Fix loss of parameters set in _SESSION

--- html/callback.php
+++ html/callback.php
@@ -1,5 +1,6 @@
 <?
 // Used to redirect to the real callback page in LODO
-// TODO: check if we lose any post params!
+// Need to start session here or we lose redirect url and data
+session_start();
 header("Location: /lodo.php?t=oauth.callback&" . http_build_query($_GET));
 ?>
