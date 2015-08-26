c41658233c50b7300c9de55043dc1795a5e95293
Mladen Drobnjakovic <mladjo2505@gmail.com>
2015-08-26 14:37:25 +0200

    Add type to arguments for xls class and move skip lines logic so we speed up import

--- modules/bank/model/xls.class.php
+++ modules/bank/model/xls.class.php
@@ -11,24 +11,24 @@ class lodo_bank_xls {
   public function __construct($file_path) {
     $this->file_path = $file_path;
   }
-  public function get_csv_array($skip_before_count = 0, $skip_after_count = 0) {
+  public function get_csv_array($skip_before_count = 0, $skip_after_count = 0, $file_type = 'application/vnd.ms-excel') {
     $Filepath = $this->file_path;
     $Sheet_Data = array();
     try {
-		  $Spreadsheet = new SpreadsheetReader($Filepath, false, 'application/vnd.ms-excel');
+		  $Spreadsheet = new SpreadsheetReader($Filepath, false, $file_type);
 		  $Sheets = $Spreadsheet->Sheets();
 		  foreach ($Sheets as $Index => $Name) {
 			  $Spreadsheet->ChangeSheet($Index);
         //var_dump($Spreadsheet);
 			  foreach ($Spreadsheet as $Key => $Row) {
           $Sheet_Row_Data = array();
-				  foreach($Row as $RowKey => $Value) {
-            if ($skip_before_count == 0) $Sheet_Row_Data[$RowKey] = $Value;
-          }
           if ($skip_before_count > 0) {
             $skip_before_count--;
           }
           else {
+            foreach($Row as $RowKey => $Value) {
+              $Sheet_Row_Data[$RowKey] = $Value;
+            }
             $Sheet_Data[] = $Sheet_Row_Data;
           }
         }
