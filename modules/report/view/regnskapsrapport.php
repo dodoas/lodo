9ad22d50a38861ba238a9afbd515b30400eb0adb
Mladen Drobnjakovic <mladjo2505@gmail.com>
2015-09-10 12:50:32 +0200

    Add sum line and a link to missing accountplans if we have some diff in regnskapsrapport

--- modules/report/view/regnskapsrapport.php
+++ modules/report/view/regnskapsrapport.php
@@ -89,6 +89,9 @@ $rapport = new framework_logic_regnskapsrapport(array('Period' => $Period, 'Star
             <th class="column_checkbox">Beregn prosent fra</th>
     <tbody>
         <?
+        $sumThisYear = 0;
+        $sumLastYear = 0;
+        $sumYear = 0;
         foreach($rapport->lineSumH as $lineH) { ?>
                 <tr>
                     <th class="sub"><? print $lineH['LineID'] ?></td>
@@ -103,6 +106,9 @@ $rapport = new framework_logic_regnskapsrapport(array('Period' => $Period, 'Star
                 </tr>
 
             <?
+            $sumThisYear += $lineH['ThisYearAmount'];
+            $sumLastYear += $lineH['LastYearAmount'];
+            $sumYear += $lineH['Year'];
             if($detail) {
                 foreach($rapport->lineH[$lineH['LineID']] as $AccountH)
                 {
@@ -123,7 +129,31 @@ $rapport = new framework_logic_regnskapsrapport(array('Period' => $Period, 'Star
             }
             }
         }
+        $sumThisYear = round($sumThisYear, 3);
+        $sumLastYear = round($sumLastYear, 3);
+        $sumYear = round($sumYear, 3);
+        if ($sumThisYear != 0 || $sumLastYear != 0 || $sumYear != 0) {
+          $link = $_lib['sess']->dispatch . "t=report.regnskapsrapportmissing&ThisYearAmount=".$sumThisYear."&LastYearAmount=".$sumLastYear."&YearAmount=".$sumYear."&StartPeriod=".$rapport->thisStartPeriod."&EndPeriod=".$rapport->thisEndPeriod;
+          $link_to_missing = "<a href='$link'>";
+          $link_to_missing_close = "</a>";
+          $warn_diff_class = "red";
+        }
+        else {
+          $link_to_missing = "";
+          $link_to_missing_close = "";
+          $warn_diff_class = "";
+        }
         ?>
+      <tr class="sub <? print $warn_diff_class; ?>">
+        <td>Sum</td>
+        <td></td>
+        <td class="number"><? print $link_to_missing . $_lib['format']->Amount($sumThisYear) . $link_to_missing_close; ?></td>
+        <td></td>
+        <td class="number"><? print $link_to_missing . $_lib['format']->Amount($sumLastYear) . $link_to_missing_close; ?></td>
+        <td></td>
+        <td class="number"><? print $link_to_missing . $_lib['format']->Amount($sumYear) . $link_to_missing_close; ?></td>
+        <td colspan="2"></td>
+    </tr>
     <tr>
         <td colspan="8"></td>
         <td colspan="3" align="right"><? print $_lib['form3']->submit(array('name'=>'show_percentcalculation', 'value'=>'Kalkuler (K)', 'accesskey'=>'K')) ?></td>
