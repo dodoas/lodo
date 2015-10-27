e3364b31e0fba3c3adce76979652c24aa3c51fad
Mladen Drobnjakovic <mladjo2505@gmail.com>
2015-10-27 13:52:58 +0100

    Add: Migration for Car

---
+++ db/changes/116_add_car_table.sql
@@ -0,0 +1,48 @@
+-- Create new tables for Car and Cars milage
+CREATE TABLE IF NOT EXISTS companycar (
+CompanyCarID int(11) NOT NULL AUTO_INCREMENT,
+CarName varchar(50) NOT NULL DEFAULT '',
+TS timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+Active tinyint(4) NOT NULL DEFAULT '0',
+ValidFrom datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+ValidTo datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+Description text,
+CarCode varchar(10) DEFAULT NULL,
+PurchasePrice decimal(16, 2) DEFAULT '0',
+PurchaseAddCost int(11) DEFAULT '0',
+PurchaseYear smallint(6) NOT NULL DEFAULT '0',
+CurrentPrice int(11) NOT NULL DEFAULT '0',
+SalePrice decimal(16, 2) DEFAULT '0',
+EnableVAT smallint(6) DEFAULT NULL,
+VehicleType varchar(50) DEFAULT '',
+PRIMARY KEY (CompanyCarID)
+);
+
+CREATE TABLE IF NOT EXISTS companycarmilage (
+CompanyCarID int(11) NOT NULL,
+MilageYear smallint(6) NOT NULL DEFAULT '0',
+km0101 int(11) DEFAULT '0',
+km3112 int(11) DEFAULT '0',
+PRIMARY KEY (CompanyCarID, MilageYear)
+);
+
+-- Add Car ID to all tables that need it
+ALTER TABLE accountplan
+ADD EnableCar smallint(6) DEFAULT NULL,
+ADD CarID bigint(20) DEFAULT 0;
+
+ALTER TABLE voucher
+ADD CarID bigint(20) DEFAULT NULL;
+
+ALTER TABLE salaryconfline
+ADD CarID bigint(20) DEFAULT NULL;
+
+ALTER TABLE salaryline
+ADD CarID bigint(20) DEFAULT NULL;
+
+ALTER TABLE accountline
+ADD CarID bigint(20) DEFAULT NULL;
+
+ALTER TABLE invoiceinline
+ADD CarID bigint(20) DEFAULT NULL;
+
