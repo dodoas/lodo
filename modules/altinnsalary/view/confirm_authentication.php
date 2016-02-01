f62144db05f1d4f6ea958687a99d13ea90be16d4
Simon <simon.toivo@telhaug.no>
2016-02-01 10:25:15 +0100

    Change fiealds to be password inputs
    on modules/altinnsalary/view/confirm_authentication.php
    and modules/altinnsalary/view/confirm_password.php

--- modules/altinnsalary/view/confirm_authentication.php
+++ modules/altinnsalary/view/confirm_authentication.php
@@ -13,7 +13,7 @@ print $_lib['sess']->doctype
 <? $target_page = $_REQUEST['request_type'] == 'feedback' ? 'altinnsalary.show4' : 'altinnsalary.list'?>
 <form name="altinnsalary_search" action="<? print $_lib['sess']->dispatch ?>t=<? print $target_page ?>" method="post">
   <input type="hidden" name="request_receivers_reference" value='<?print $_REQUEST['request_receivers_reference']; ?>'>
-  <? print $_lib['form3']->text(array('name'=>'user_pin_code', 'width'=>'10' , 'value'=>'')) ?>
+  <? print $_lib['form3']->input(array('name'=>'user_pin_code', 'type'=>'password', 'width'=>'10' , 'value'=>'')) ?>
     <?
     if($_REQUEST['request_type'] == 'feedback'){
         print $_lib['form3']->submit(array('name'=>'action_soap4', 'value'=>'Get Feedback'));
--- modules/altinnsalary/view/confirm_password.php
+++ modules/altinnsalary/view/confirm_password.php
@@ -16,7 +16,7 @@ Skriv inn passord for &aring; logge inn i altinn
 <form name="altinnsalary_search" action="<? print $_lib['sess']->dispatch ?>t=<? print $target_page ?>" method="post">
   <input type="hidden" name="request_receivers_reference" value='<?print $_REQUEST['request_receivers_reference']; ?>'>
   <input type="hidden" name="request_type" value='<?print $_REQUEST['request_type']; ?>'>
-  <? print $_lib['form3']->text(array('name'=>'user_pass_code', 'value'=>'')) ?>
+  <? print $_lib['form3']->input(array('name'=>'user_pass_code', 'type'=>'password','value'=>'')) ?>
     <?
     if($_REQUEST['request_type'] == 'feedback'){
       print $_lib['form3']->submit(array('name'=>'action_soap4', 'value'=>'Get Feedback'));
