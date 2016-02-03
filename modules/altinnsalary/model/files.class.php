bcb20103dbcd8f088109ce2dc7e14bcda3470c4b
Simon <simon.toivo@telhaug.no>
2016-02-03 09:13:57 +0100

    Restucture altinn file structure

--- modules/altinnsalary/model/files.class.php
+++ modules/altinnsalary/model/files.class.php
@@ -1,37 +1,36 @@
 <?php
 class altinn_file {
-  function __construct( $folder = false) {
+  function __construct($date) {
     global $_SETUP;
-    if (!$folder)
-      $folder = (string) time();
 
     // TODO change this path to somewhere outside repo
-    $this->folderPath = $_SETUP['HOME_DIR']."/xml/".$folder;
+    $this->folderPath = $_SETUP['HOME_DIR']."/altinn_xml/".$_SETUP['DB_NAME'].'/'.$date.'/';
   }
 
-  function save($content) {
+  function save($content, $id) {
     global $_lib;
     // make folder to work in
     self::makeFolder($this->folderPath);
 
     // write file to disk
-    return self::writeFile("/A-melding.xml", $content);
+    return self::writeFile('req_'.$id.'.xml', $content);
   }
 
-  function extract($zipContent) {
+  function extract($zipContent, $id) {
     global $_lib;
     // make folder to work in
     self::makeFolder($this->folderPath);
 
     // Save the ziped file to disk
-    $file = self::writeFile("/tilbakemelding.zip", $zipContent);
+    $file = self::writeFile("tilbakemelding".$id.".zip", $zipContent);
 
     // unzip the file. Need -d(esitination folder) otherwise it will
     // will create them in in current folder. I this case '~/'.
-    exec("unzip ".$this->folderPath. "/tilbakemelding.zip -d ".$this->folderPath);
+    exec("unzip ".$this->folderPath. "tilbakemelding".$id.".zip -d ".$this->folderPath);
+    exec("mv ".$this->folderPath. "tilbakemelding.xml ". $this->folderPath . 'tilbakemelding'.$id.".xml");
 
     // Read the xml from file
-    $xmlContent = self::readFile("/tilbakemelding.xml");
+    $xmlContent = self::readFile("tilbakemelding".$id.".xml");
     return $xmlContent;
   }
 
