7351784836186898ac423e6f822119c75d692f97
Baxxx <branislav.dodo@gmail.com>
2017-02-20 14:36:53 +0100

    Remove all NO:ORGNR 123123123 stuff. Disable sending if no firmaid

---
+++ db/changes/166_remove_orgno_123123123.sql
@@ -0,0 +1,11 @@
+UPDATE accountplan SET OrgNumber = '' WHERE OrgNumber = '123123123';
+UPDATE accountplan SET VatNumber = '' WHERE VatNumber = '123123123';
+DELETE FROM accountplanscheme WHERE SchemeValue = '123123123' AND FakturabankSchemeID IN ((SELECT FakturabankSchemeID FROM fakturabankscheme WHERE SchemeType IN ('NO:ORGNR', 'NO:VAT')), 0, NULL);
+
+UPDATE accountline SET ReskontroOrgNumber = '' WHERE ReskontroOrgNumber = '123123123';
+
+UPDATE fakturabanktransactionrelation SET AccountPlanOrgNumber = NULL WHERE AccountPlanOrgNumber = 123123123;
+
+UPDATE fakturabankinvoiceout SET CustomerPartyIndentification = '' WHERE CustomerPartyIndentification = '123123123';
+UPDATE invoiceout SET IOrgNo = '' WHERE IOrgNo = '123123123';
+UPDATE recurringout SET IOrgNo = '' WHERE IOrgNo = '123123123';
