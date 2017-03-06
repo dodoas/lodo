39f95c63c4a538800668233cdf8e288b10d23f22
Baxxx <branislav.dodo@gmail.com>
2017-03-06 09:40:38 +0100

    Fix migration 166 for 'Subquery returns multiple rows'

--- db/changes/166_remove_orgno_123123123.sql
+++ db/changes/166_remove_orgno_123123123.sql
@@ -1,6 +1,6 @@
 UPDATE accountplan SET OrgNumber = '' WHERE OrgNumber = '123123123';
 UPDATE accountplan SET VatNumber = '' WHERE VatNumber = '123123123';
-DELETE FROM accountplanscheme WHERE SchemeValue = '123123123' AND FakturabankSchemeID IN ((SELECT FakturabankSchemeID FROM fakturabankscheme WHERE SchemeType IN ('NO:ORGNR', 'NO:VAT')), 0, NULL);
+DELETE FROM accountplanscheme WHERE SchemeValue = '123123123' AND FakturabankSchemeID IN ((SELECT FakturabankSchemeID FROM fakturabankscheme WHERE SchemeType = 'NO:ORGNR'), (SELECT FakturabankSchemeID FROM fakturabankscheme WHERE SchemeType = 'NO:VAT'), 0, NULL);
 
 UPDATE accountline SET ReskontroOrgNumber = '' WHERE ReskontroOrgNumber = '123123123';
 
